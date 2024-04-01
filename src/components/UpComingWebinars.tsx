bda98fdefeb0da183e62ccae053db88d646afdc7
rohitcodism <rohitpaula18i@gmail.com>
2024-04-01 12:03:04 +0530

    ✅ Add link to view all upcoming events

--- src/components/UpComingWebinars.tsx
+++ src/components/UpComingWebinars.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { HoverEffect } from './ui/card-hover-effect';
+import Link from 'next/link';
 
 const events = [
     {
@@ -57,6 +58,22 @@ function UpComingWebinars() {
                 Upcoming Webinars
             </h2>
             <HoverEffect items={events} />
+            <div
+                className='
+                    py-4
+                '
+            >
+                <Link
+                    href={"/all-events"}
+                >
+                    <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl p-4">
+                            View all upcoming events
+                        </span>
+                    </button>
+                </Link>
+            </div>
         </div>
     )
 }
